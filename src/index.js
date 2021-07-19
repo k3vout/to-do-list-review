45d5d15ae2a1edf7f23a444225077e6a3bc5cc10
Kevin <kevinsevinche@gmail.com>
2021-07-19 16:59:35 -0500

    Fix Linter Errors

--- src/index.js
+++ src/index.js
@@ -1,54 +1,54 @@
-import "./style.css";
-import DragAndSort from "./drag.js";
-import Status from "./status.js";
-import Crud from "./crud.js";
+import './style.css';
+import DragAndSort from './drag.js';
+import Status from './status.js';
+import Crud from './crud.js';
 
 const list = [];
 
 function loadLiEvents() {
-  const liElements = document.querySelectorAll(".item");
+  const liElements = document.querySelectorAll('.item');
   liElements.forEach((element) => {
     const drag = new DragAndSort();
-    element.addEventListener("dragstart", drag.dragStart);
-    element.addEventListener("dragover", drag.dragOver);
-    element.addEventListener("drop", drag.drop);
+    element.addEventListener('dragstart', drag.dragStart);
+    element.addEventListener('dragover', drag.dragOver);
+    element.addEventListener('drop', drag.drop);
   });
-  const itemDesc = document.querySelectorAll(".item-description");
+  const itemDesc = document.querySelectorAll('.item-description');
   itemDesc.forEach((item) => {
     const crud = new Crud();
-    item.addEventListener("input", crud.updateTask); // Update Elements
-    item.addEventListener("focusin", crud.showTrashAndRemove); // Remove Elements
-    item.addEventListener("focusout", crud.showDots);
+    item.addEventListener('input', crud.updateTask); // Update Elements
+    item.addEventListener('focusin', crud.showTrashAndRemove); // Remove Elements
+    item.addEventListener('focusout', crud.showDots);
   });
 }
 
-document.addEventListener("DOMContentLoaded", loadLiEvents);
+document.addEventListener('DOMContentLoaded', loadLiEvents);
 
 function loadCheckboxes() {
-  const checkboxes = document.querySelectorAll(".checks");
+  const checkboxes = document.querySelectorAll('.checks');
   checkboxes.forEach((checkbox) => {
     const status = new Status();
-    checkbox.addEventListener("change", status.validation);
+    checkbox.addEventListener('change', status.validation);
   });
 }
 
-document.addEventListener("DOMContentLoaded", loadCheckboxes);
+document.addEventListener('DOMContentLoaded', loadCheckboxes);
 
 // LOCAL STORAGE
 
 if (!localStorage.ToDoList) {
-  document.addEventListener("DOMContentLoaded", DragAndSort.sortList(list));
+  document.addEventListener('DOMContentLoaded', DragAndSort.sortList(list));
 } else {
   document.addEventListener(
-    "DOMContentLoaded",
-    DragAndSort.sortList(JSON.parse(localStorage.getItem("ToDoList")))
+    'DOMContentLoaded',
+    DragAndSort.sortList(JSON.parse(localStorage.getItem('ToDoList'))),
   );
 }
 
 // ADD ELEMENTS
-const newItem = document.getElementById("add-to-list");
+const newItem = document.getElementById('add-to-list');
 const newCrud = new Crud();
-newItem.addEventListener("keypress", newCrud.addToList);
+newItem.addEventListener('keypress', newCrud.addToList);
 // Remove All
-const btnClear = document.getElementById("clear");
-btnClear.addEventListener("click", newCrud.clearCompleted);
+const btnClear = document.getElementById('clear');
+btnClear.addEventListener('click', newCrud.clearCompleted);
--- src/status.js
+++ src/status.js
@@ -1,13 +1,13 @@
 export default class Status {
   validation() {
     if (this.checked) {
-      this.nextSibling.style["text-decoration"] = "line-through";
-      this.nextSibling.style.color = "#909090";
+      this.nextSibling.style['text-decoration'] = 'line-through';
+      this.nextSibling.style.color = '#909090';
       // Save in Local Storage
       Status.saveChanges();
     } else {
-      this.nextSibling.style["text-decoration"] = "none";
-      this.nextSibling.style.color = "#000";
+      this.nextSibling.style['text-decoration'] = 'none';
+      this.nextSibling.style.color = '#000';
       // Save in Local Storage
       Status.saveChanges();
     }
@@ -15,7 +15,7 @@ export default class Status {
 
   static saveChanges() {
     const newList = [];
-    const listLi = document.querySelectorAll(".item");
+    const listLi = document.querySelectorAll('.item');
     listLi.forEach((li, i) => {
       newList.push({
         index: i + 1,
@@ -23,6 +23,6 @@ export default class Status {
         completed: li.firstChild.firstChild.checked,
       });
     });
-    localStorage.setItem("ToDoList", JSON.stringify(newList));
+    localStorage.setItem('ToDoList', JSON.stringify(newList));
   }
 }
